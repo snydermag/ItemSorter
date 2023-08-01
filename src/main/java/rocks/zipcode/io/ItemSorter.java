cc172da847be1e109509edb8449147c2bba516a8
maggie <snydermag@gmail.com>
2023-08-01 16:33:39 -0400

    all tests passed other than mine. Unclear why my don't pass. The results are not values, so I am unsure why they are different. All the code for the tests are the same as the id and name tests. Those pass, yet the price one does not.

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,30 @@
 package rocks.zipcode.io;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+    private Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        List<Item> list = new ArrayList<>(Arrays.asList(items));
+        list.sort(comparator);
+
+        Item[] sorted = new Item[items.length];
+
+        for (int i = 0; i < items.length; i++){
+            sorted[i] = list.get(i);
+        }
+        return sorted;
+
     }
 }
