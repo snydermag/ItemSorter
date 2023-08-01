6f0ad350cd1fda78d18b20d1d2fd79e8124f2da1
maggie <snydermag@gmail.com>
2023-08-01 14:14:12 -0400

    item tests pass

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,43 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+
+    private String expectedName;
+    private Long expectedId;
+    private Double expectedPrice;
     public Item() {
+        expectedName = "";
+        expectedId = 0L;
+        expectedPrice = 0.01;
     }
 
     public Item(Long id, String name, Double price) {
+        expectedName = name;
+        expectedId = id;
+        expectedPrice = price;
     }
 
     public String getName() {
-        return null;
+        return expectedName;
     }
 
     public void setName(String name) {
+        expectedName = name;
     }
 
     public Long getId() {
-        return null;
+        return expectedId;
     }
 
     public void setId(Long id) {
+        expectedId = id;
     }
 
     public Double getPrice() {
-        return null;
+        return expectedPrice;
     }
 
     public void setPrice(Double price) {
+        expectedPrice = price;
     }
 }
